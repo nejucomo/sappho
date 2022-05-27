1506b8f9ee7e79c61d8efe73d3cb0366cdd6f66e
Nate Wilcox <nathan+dev@electriccoin.co>
2022-05-27 10:19:20 -0700

    Split error mod between set and `Error` type; display `Error n/m: ` prefixes.

--- parser/src/error.rs
+++ parser/src/error.rs
@@ -1,40 +1,5 @@
-use derive_more::From;
-use std::fmt;
+mod etype;
+mod set;
 
-#[derive(Debug, From)]
-pub struct ErrorSet<T>(Vec<T>);
-
-pub type Errors = ErrorSet<Error>;
-pub type Error = chumsky::error::Simple<char>;
-
-impl<T> ErrorSet<T> {
-    pub fn push(&mut self, error: T) {
-        self.0.push(error)
-    }
-
-    pub fn into_result(self) -> Result<(), Self> {
-        if self.0.is_empty() {
-            Ok(())
-        } else {
-            Err(self)
-        }
-    }
-}
-
-impl<T> Default for ErrorSet<T> {
-    fn default() -> Self {
-        ErrorSet(vec![])
-    }
-}
-
-impl<T> fmt::Display for ErrorSet<T>
-where
-    T: fmt::Display,
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for error in self.0.iter() {
-            write!(f, "\n{}\n", error)?;
-        }
-        Ok(())
-    }
-}
+pub use self::etype::Error;
+pub use self::set::{ErrorSet, Errors};
---
+++ parser/src/error/set.rs
@@ -0,0 +1,42 @@
+use crate::Error;
+
+use derive_more::From;
+use std::fmt;
+
+#[derive(Debug, From)]
+pub struct ErrorSet<T>(Vec<T>);
+
+pub type Errors = ErrorSet<Error>;
+
+impl<T> ErrorSet<T> {
+    pub fn push(&mut self, error: T) {
+        self.0.push(error)
+    }
+
+    pub fn into_result(self) -> Result<(), Self> {
+        if self.0.is_empty() {
+            Ok(())
+        } else {
+            Err(self)
+        }
+    }
+}
+
+impl<T> Default for ErrorSet<T> {
+    fn default() -> Self {
+        ErrorSet(vec![])
+    }
+}
+
+impl<T> fmt::Display for ErrorSet<T>
+where
+    T: fmt::Display,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let tot = self.0.len();
+        for (i, error) in self.0.iter().enumerate() {
+            writeln!(f, "Error {}/{}: {}", i + 1, tot, error)?;
+        }
+        Ok(())
+    }
+}
