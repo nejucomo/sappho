e6e52bf46a6ed6ca645869da50a2f2793e161380
Nate Wilcox <nathan+dev@electriccoin.co>
2022-05-28 08:09:36 -0700

    [no-verify] Implement basic complete error display.

--- parser/src/error/etype.rs
+++ parser/src/error/etype.rs
@@ -38,6 +38,66 @@ impl ChumskyError<char> for Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.0.fmt(f)
+        use chumsky::error::SimpleReason::*;
+
+        let labeldesc = if let Some(label) = self.0.label() {
+            format!(", while parsing {}", label)
+        } else {
+            "".to_string()
+        };
+
+        write!(f, "at {:?}{}: ", self.0.span(), labeldesc)?;
+
+        match self.0.reason() {
+            Unexpected => write!(
+                f,
+                "found {}, expected {}",
+                describe_found(self.0.found().map(|&c| c)),
+                comma_separated_or(self.0.expected().map(|&oc| describe_found(oc))),
+            ),
+            Unclosed { span, delimiter } => write!(f, "unclosed {:?} at {:?}", delimiter, span),
+            Custom(msg) => write!(f, "{}", msg),
+        }
+    }
+}
+
+fn describe_found(oc: Option<char>) -> String {
+    oc.map(|c| format!("{:?}", c))
+        .unwrap_or_else(|| "nothing".to_string())
+}
+
+fn comma_separated_or<I>(mut it: I) -> String
+where
+    I: Iterator<Item = String>,
+{
+    let mut s = String::new();
+    if let Some(mut prev) = it.next() {
+        let mut ix = 0;
+        while let Some(x) = it.next() {
+            if ix > 0 {
+                s.push_str(", ");
+            }
+            s.push_str(&prev);
+            prev = x;
+            ix += 1;
+        }
+        if !s.is_empty() {
+            s.push_str(", or ");
+        }
+        s.push_str(&prev);
+    }
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+
+    #[test_case(&[] => "".to_string())]
+    #[test_case(&["a"] => "a".to_string())]
+    #[test_case(&["a", "b"] => "a, or b".to_string())]
+    #[test_case(&["a", "b", "c"] => "a, b, or c".to_string())]
+    fn comma_separated_or(xs: &[&str]) -> String {
+        super::comma_separated_or(xs.iter().map(|x| x.to_string()))
     }
 }
