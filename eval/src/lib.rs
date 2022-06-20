fca1a5ae14f835817126d34543928baedd09eb2b
Nate Wilcox <nathan+dev@electriccoin.co>
2022-06-20 11:44:48 -0700

    Upgrade `ValRef` to a newtype.

--- eval/src/lib.rs
+++ eval/src/lib.rs
@@ -4,6 +4,7 @@ mod eval;
 mod list;
 mod object;
 mod scope;
+mod valref;
 mod value;
 
 pub(crate) use self::bind::bind;
@@ -11,7 +12,8 @@ pub use self::error::{Error, Result};
 pub use self::eval::eval;
 pub use self::list::List;
 pub use self::object::Object;
-pub use self::value::{ValRef, Value};
+pub use self::valref::ValRef;
+pub use self::value::Value;
 
 #[cfg(test)]
 mod tests;
