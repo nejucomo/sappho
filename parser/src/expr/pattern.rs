61e7b1ddfa141e237f5259fef5d93d7bfacdedfd
Nate Wilcox <nathan+dev@electriccoin.co>
2022-06-21 16:52:46 -0700

    Implement literal pattern parsing.

--- parser/src/expr/pattern.rs
+++ parser/src/expr/pattern.rs
@@ -1,7 +1,10 @@
 use crate::error::BareError;
+use crate::expr::universal::literal;
 use chumsky::{text, Parser};
 use sappho_ast::Pattern;
 
 pub(crate) fn pattern() -> impl Parser<char, Pattern, Error = BareError> {
-    text::ident().map(Pattern::Bind)
+    use Pattern::*;
+
+    text::ident().map(Bind).or(literal().map(LitEq))
 }
