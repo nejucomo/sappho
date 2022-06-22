5695468f7294bb6daf3e3e7f6115f899634f42e2
Nate Wilcox <nathan+dev@electriccoin.co>
2022-06-22 15:56:37 -0700

    Add integration test that uncovers UX bug in coercion errors; fix w/ `Coerce::sappho_type_name`.

--- value/src/coerce/failure.rs
+++ value/src/coerce/failure.rs
@@ -1,17 +1,21 @@
-use crate::ValRef;
+use crate::{Coerce, ValRef};
 use std::fmt;
 
 #[derive(Debug)]
 pub struct CoercionFailure(ValRef, &'static str);
 
 impl CoercionFailure {
-    pub fn new<T>(v: &ValRef) -> Self {
-        CoercionFailure(v.clone(), std::any::type_name::<T>())
+    pub fn new<T>(v: &ValRef) -> Self
+    where
+        T: Coerce,
+    {
+        CoercionFailure(v.clone(), T::sappho_type_name())
     }
 }
 
 impl fmt::Display for CoercionFailure {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "unbound: {:?}", &self.0)
+        let CoercionFailure(v, typename) = self;
+        write!(f, "cannot coerce {} into {}", v, typename)
     }
 }
