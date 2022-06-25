6024c8d444d437bdaf7e5ffea7fd82130c5a6a2e
Nate Wilcox <nathan+dev@electriccoin.co>
2022-06-25 08:10:46 -0700

    Switch logging provider; log to stderr.

--- cli/src/log.rs
+++ cli/src/log.rs
@@ -1,9 +1,11 @@
 pub use log::SetLoggerError;
 
 pub fn init() -> Result<(), SetLoggerError> {
-    simple_logger::SimpleLogger::new()
-        .with_level(log::LevelFilter::Trace)
-        .init()?;
+    use log::LevelFilter::Trace;
+    use simplelog::{ColorChoice, Config, TermLogger, TerminalMode::Stderr};
+
+    TermLogger::init(Trace, Config::default(), Stderr, ColorChoice::Auto)?;
+
     log::debug!("Log initialized.");
     Ok(())
 }
