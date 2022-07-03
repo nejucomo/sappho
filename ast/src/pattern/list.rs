e767df20c1889f81e94a2210c98f00643e78261d
Nate Wilcox <nathan+dev@electriccoin.co>
2022-07-03 07:37:40 -0700

    [no-verify] Add tail matching and `Display` to `sappho_ast::ListPattern`.

--- ast/src/pattern/list.rs
+++ ast/src/pattern/list.rs
@@ -1,33 +1,30 @@
-use crate::Pattern;
+use crate::{Identifier, Pattern};
 use std::fmt;
-use std::ops::Deref;
 
 #[derive(Clone, Debug, PartialEq, derive_more::From)]
-pub struct ListPattern(Vec<Pattern>);
+pub struct ListPattern {
+    pub body: Vec<Pattern>,
+    pub tail: Option<Identifier>,
+}
+
+impl ListPattern {
+    pub fn new<I>(body: I, tail: Option<Identifier>) -> Self
+    where
+        I: IntoIterator<Item = Pattern>,
+    {
+        ListPattern {
+            body: body.into_iter().collect(),
+            tail,
+        }
+    }
+}
 
 impl FromIterator<Pattern> for ListPattern {
     fn from_iter<I>(iter: I) -> Self
     where
         I: IntoIterator<Item = Pattern>,
     {
-        ListPattern(iter.into_iter().collect())
-    }
-}
-
-impl IntoIterator for ListPattern {
-    type IntoIter = <Vec<Pattern> as IntoIterator>::IntoIter;
-    type Item = Pattern;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
-
-impl Deref for ListPattern {
-    type Target = [Pattern];
-
-    fn deref(&self) -> &[Pattern] {
-        &self.0[..]
+        ListPattern::new(iter, None)
     }
 }
 
@@ -35,8 +32,33 @@ impl fmt::Display for ListPattern {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use sappho_fmtutil::fmt_comma_sep;
         write!(f, "[")?;
-        fmt_comma_sep(self.iter(), f)?;
+        fmt_comma_sep(self.body.iter(), f)?;
+        if let Some(tail) = &self.tail {
+            if !self.body.is_empty() {
+                write!(f, ", ")?;
+            }
+            write!(f, "..{}", tail)?;
+        }
         write!(f, "]")?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{ListPattern, Pattern};
+    use test_case::test_case;
+
+    #[test_case([], None => "[]")]
+    #[test_case([], Some("t") => "[..t]")]
+    #[test_case(["h"], None => "[h]")]
+    #[test_case(["h"], Some("t") => "[h, ..t]")]
+    #[test_case(["a", "b"], Some("t") => "[a, b, ..t]")]
+    fn display<const K: usize>(body: [&str; K], tail: Option<&str>) -> String {
+        ListPattern::new(
+            body.map(|s| Pattern::Bind(s.to_string())),
+            tail.map(|s| s.to_string()),
+        )
+        .to_string()
     }
 }
