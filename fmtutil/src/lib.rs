c0f499ac1014c6e815cb5f97ada3dcc667e2f43d
Nate Wilcox <nathan+dev@electriccoin.co>
2022-07-12 14:07:09 -0600

    Remove unused code in `sappho-fmtutil`.

--- fmtutil/src/lib.rs
+++ fmtutil/src/lib.rs
@@ -1,5 +1,28 @@
-mod ct;
-mod dd;
+use std::fmt::Display;
+pub use std::fmt::{Formatter, Result as FmtResult};
 
-pub use self::ct::{fmt_comma_sep, CommaTracker};
-pub use self::dd::{indent, DisplayDepth, FmtResult, Formatter};
+pub trait DisplayDepth {
+    fn fmt_depth(&self, f: &mut Formatter, depth: usize) -> FmtResult;
+}
+
+pub fn indent(f: &mut Formatter, depth: usize) -> FmtResult {
+    for _ in 0..depth {
+        write!(f, "  ")?;
+    }
+    Ok(())
+}
+
+impl DisplayDepth for String {
+    fn fmt_depth(&self, f: &mut Formatter, _depth: usize) -> FmtResult {
+        self.fmt(f)
+    }
+}
+
+impl<X> DisplayDepth for Box<X>
+where
+    X: DisplayDepth,
+{
+    fn fmt_depth(&self, f: &mut Formatter, depth: usize) -> FmtResult {
+        self.as_ref().fmt_depth(f, depth)
+    }
+}
