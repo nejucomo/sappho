bb4e5fb1b8933e68eb9a3524dca729e3ae039628
Nate Wilcox <nathan+dev@electriccoin.co>
2022-07-13 13:58:12 -0600

    [no-verify; tests build] First draft backwards-compatible implementation of `Display for Stream`.

--- unparse/src/stream/display.rs
+++ unparse/src/stream/display.rs
@@ -1,8 +1,30 @@
+use super::Item::{self, *};
 use crate::Stream;
 use std::fmt;
 
 impl fmt::Display for Stream {
-    fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
-        todo!();
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt_items(self, f, 0)
+    }
+}
+
+fn fmt_items(stream: &Stream, f: &mut fmt::Formatter, depth: usize) -> fmt::Result {
+    for item in &stream.0[..] {
+        fmt_item(item, f, depth)?;
+    }
+    Ok(())
+}
+
+fn fmt_item(item: &Item, f: &mut fmt::Formatter, depth: usize) -> fmt::Result {
+    match item {
+        Leaf(s) => f.write_str(&s),
+        Break(_) => {
+            f.write_str("\n")?;
+            for _ in 0..depth {
+                f.write_str("  ")?;
+            }
+            Ok(())
+        }
+        Substream(sub) => fmt_items(sub, f, depth + 1),
     }
 }
