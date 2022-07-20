ef4173f994fe41356905bd54c825167590679d07
Nate Wilcox <nathan+dev@electriccoin.co>
2022-07-20 09:57:09 -0700

    [no-verify] Add proc to `sappho-ast`.

--- ast/src/lib.rs
+++ ast/src/lib.rs
@@ -12,7 +12,8 @@ mod expr;
 mod pattern;
 
 pub type ApplicationExpr<FX> = sappho_ast_core::ApplicationExpr<Expr<FX>>;
-pub type CoreExpr<FX> = sappho_ast_core::CoreExpr<Pattern, PureExpr, QueryExpr, Expr<FX>, FX>;
+pub type CoreExpr<FX> =
+    sappho_ast_core::CoreExpr<Pattern, PureExpr, QueryExpr, ProcExpr, Expr<FX>, FX>;
 pub type EffectExpr<FX> = sappho_ast_core::EffectExpr<FX, Expr<FX>>;
 pub type FuncDef = sappho_ast_core::FuncDef<Pattern, PureExpr>;
 pub type LetClause<FX> = sappho_ast_core::LetClause<Pattern, Expr<FX>>;
@@ -21,7 +22,8 @@ pub type ListExpr<FX> = sappho_listform::ListForm<Expr<FX>, Box<Expr<FX>>>;
 pub type LookupExpr<FX> = sappho_ast_core::LookupExpr<Expr<FX>>;
 pub type MatchClause<FX> = sappho_ast_core::MatchClause<Pattern, Expr<FX>>;
 pub type MatchExpr<FX> = sappho_ast_core::MatchExpr<Pattern, Expr<FX>>;
-pub type ObjectDef<FX> = sappho_ast_core::ObjectDef<Pattern, PureExpr, QueryExpr, Expr<FX>>;
+pub type ObjectDef<FX> =
+    sappho_ast_core::ObjectDef<Pattern, PureExpr, QueryExpr, ProcExpr, Expr<FX>>;
 pub type ProcExpr = Expr<sappho_ast_core::ProcEffects>;
 pub type PureExpr = Expr<sappho_ast_core::PureEffects>;
 pub type QueryDef = sappho_ast_core::QueryDef<QueryExpr>;
