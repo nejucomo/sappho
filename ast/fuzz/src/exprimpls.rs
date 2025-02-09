b51496c32aeac1097d2a08df584cf7824dc10cae
Nate Wilcox <nejucomo+dev@gmail.com>
2025-02-08 16:56:49 -0800

    Add `Expr::Query` generator.

--- ast/fuzz/src/exprimpls.rs
+++ ast/fuzz/src/exprimpls.rs
@@ -7,8 +7,9 @@ use crate::AstFuzz;
 
 impl<FX> Distribution<Expr<FX>> for AstFuzz {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Expr<FX> {
-        self.map(Expr::Proc)
+        self.map(Expr::Query)
             .weighted_case(1)
+            .or(self.map(Expr::Proc).weighted_case(1))
             .or(self.map(Expr::List).weighted_case(1))
             .sample(rng)
     }
--- ast/fuzz/src/fqpimpls.rs
+++ ast/fuzz/src/fqpimpls.rs
@@ -1,8 +1,14 @@
 use rand::distr::Distribution;
 use rand::Rng;
-use sappho_ast::{ProcDef, Statements};
+use sappho_ast::{ProcDef, QueryDef, Statements};
 
 use crate::AstFuzz;
+
+impl Distribution<QueryDef> for AstFuzz {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> QueryDef {
+        QueryDef::new(rng.sample(self))
+    }
+}
 
 impl Distribution<ProcDef> for AstFuzz {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> ProcDef {
