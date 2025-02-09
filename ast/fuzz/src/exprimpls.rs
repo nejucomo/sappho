420fd352cc5f2c86804023333a988a3efb1afeda
Nate Wilcox <nejucomo+dev@gmail.com>
2025-02-08 17:12:15 -0800

    Start incomplete `Expr::Core` generation.

--- ast/fuzz/src/exprimpls.rs
+++ ast/fuzz/src/exprimpls.rs
@@ -1,17 +1,31 @@
 use rand::distr::Distribution;
 use rand::Rng;
-use sappho_ast::Expr;
+use sappho_ast::{CoreExpr, Expr};
 use sappho_rand_dcomp::{DistributionExt, WeightedCase};
 
 use crate::AstFuzz;
 
 impl<FX> Distribution<Expr<FX>> for AstFuzz {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Expr<FX> {
-        self.map(Expr::Func)
+        use Expr::*;
+
+        self.map(Core)
             .weighted_case(1)
-            .or(self.map(Expr::Query).weighted_case(1))
-            .or(self.map(Expr::Proc).weighted_case(1))
-            .or(self.map(Expr::List).weighted_case(1))
+            .or(self.map(Func).weighted_case(1))
+            .or(self.map(Query).weighted_case(1))
+            .or(self.map(Proc).weighted_case(1))
+            .or(self.map(List).weighted_case(1))
+            .sample(rng)
+    }
+}
+
+impl<FX> Distribution<CoreExpr<FX>> for AstFuzz {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> CoreExpr<FX> {
+        use sappho_ast_core::CoreExpr::*;
+
+        self.map(Lit)
+            .weighted_case(1)
+            .or(self.map(Ref).weighted_case(1))
             .sample(rng)
     }
 }
