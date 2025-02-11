e2e2a0d7f8d18bdb2d26ed75d9991f1009318254
Nate Wilcox <nejucomo+dev@gmail.com>
2025-02-11 09:30:50 -0800

    [no-verify] Update `...-value` to new AST API.

--- value/src/thunk.rs
+++ value/src/thunk.rs
@@ -1,5 +1,8 @@
-use crate::ScopeRef;
+use derive_new::new;
+use sappho_ast_core::Effect;
 use sappho_ast_reduced::Expr;
+
+use crate::ScopeRef;
 
 /// Bind a source expression to the runtime scope in which it appears for later evaluation.
 ///
@@ -16,16 +19,19 @@ use sappho_ast_reduced::Expr;
 /// When the value of `r` is evaluated a thunk captures the expression `$q` as well as the scope
 /// containing `q`. When `$r` is evaluated, the thunk of the `r` query is evaluated with it's
 /// definition scope.
-pub struct GenThunk<Effect> {
-    expr: Expr<Effect>,
+#[derive(Debug, new)]
+pub struct GenThunk<FX>
+where
+    FX: Effect,
+{
+    expr: Expr<FX>,
     scope: ScopeRef,
 }
 
-impl<FX> GenThunk<FX> {
-    pub fn new(expr: Expr<FX>, scope: ScopeRef) -> Self {
-        GenThunk { scope, expr }
-    }
-
+impl<FX> GenThunk<FX>
+where
+    FX: Effect,
+{
     pub fn peek(&self) -> (&Expr<FX>, &ScopeRef) {
         (&self.expr, &self.scope)
     }
