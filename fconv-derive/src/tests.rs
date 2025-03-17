4d6f1b645a518a55965055f3ae87bbb2c4152e1a
Nate Wilcox <nejucomo+dev@gmail.com>
2025-03-17 13:42:07 -0700

    [no-verify] Start on matrix test cases for `-fconv-derive`.

--- fconv-derive/src/tests.rs
+++ fconv-derive/src/tests.rs
@@ -1,17 +1,41 @@
 use indoc::indoc;
 use proc_macro2::TokenStream;
-use test_case::test_case;
+use test_case::{test_case, test_matrix};
 
-struct TestCase {
-    input: &'static str,
-    expected: &'static str,
+struct TestCase<S> {
+    input: S,
+    expected: S,
 }
 
-// We want to test a cartesian produce:
-// - struct-vs-enum
-// - indexed-vs-named fields
-// - single-field-vs-multi-field (? via tuples like `derive_more::From` ?)
-// - concrete-vs-generic-with-bounds
+impl<S> TestCase<S>
+where
+    S: AsRef<str>,
+{
+    fn as_strs(&self) -> TestCase<&str> {
+        TestCase {
+            input: self.input.as_ref(),
+            expected: self.expected.as_ref(),
+        }
+    }
+}
+
+impl TestCase<&str> {
+    fn check_expansion(self) {
+        let TestCase { input, expected } = self;
+        let input_tokens = parse_tokens(input);
+        let expected_tokens = parse_tokens(expected);
+        let actual_tokens = crate::extract_derive_pm2(input_tokens);
+        let actual = actual_tokens.to_string();
+
+        assert_eq!(
+            // This is normalized compared to `expected`:
+            expected_tokens.to_string(),
+            actual,
+            "Expected:\n{expected}\n\nActual:\n{actual}\n\n"
+        );
+    }
+}
+
 #[test_case(
     TestCase {
         input: indoc! { r#"
@@ -81,18 +105,114 @@ struct TestCase {
     }
     ; "concrete-enum-1-indexed-fields"
 )]
-fn extract_derive_pm2(TestCase { input, expected }: TestCase) {
-    let input_tokens = parse_tokens(input);
-    let expected_tokens = parse_tokens(expected);
-    let actual_tokens = crate::extract_derive_pm2(input_tokens);
-    let actual = actual_tokens.to_string();
+fn extract_derive_pm2(tc: TestCase<&str>) {
+    tc.check_expansion()
+}
 
-    assert_eq!(
-        // This is normalized compared to `expected`:
-        expected_tokens.to_string(),
-        actual,
-        "Expected:\n{expected}\n\nActual:\n{actual}\n\n"
-    );
+// We want to test a cartesian produce:
+// - struct-vs-enum
+// - indexed-vs-named fields
+// - single-field-vs-multi-field (? via tuples like `derive_more::From` ?)
+// - concrete-vs-generic-with-bounds
+
+// Test Matrix Axes Types:
+#[derive(Debug, Copy, Clone)]
+enum Kind {
+    Struct,
+    Enum,
+}
+use Kind::*;
+
+#[derive(Debug, Copy, Clone)]
+enum FieldSpec {
+    Indexed,
+    Named,
+}
+use FieldSpec::*;
+
+#[derive(Debug, Copy, Clone)]
+enum Genericity {
+    Concrete,
+    Generic,
+}
+use Genericity::*;
+
+// Maybe TODO?
+// enum FieldCount { Single, Multiple }
+
+#[test_matrix(
+    [Struct, Enum],
+    [Indexed, Named],
+    [Concrete, Generic]
+)]
+fn matrix(kind: Kind, fspec: FieldSpec, gen: Genericity) {
+    TestCase {
+        input: matrix_input(kind, fspec, gen),
+        expected: matrix_expected(kind, fspec, gen),
+    }
+    .as_strs()
+    .check_expansion()
+}
+
+fn matrix_input(kind: Kind, fspec: FieldSpec, gen: Genericity) -> String {
+    let mut input = "".to_string();
+    input.push_str(match kind {
+        Struct => "struct",
+        Enum => "enum",
+    });
+    input.push_str(" Foo");
+    if matches!(gen, Generic) {
+        input.push_str("<T>");
+    }
+    input.push_str(match (kind, fspec) {
+        (Struct, Indexed) => "(T);",
+        (Struct, Named) => indoc! { r#"
+             {
+              t: T
+            }
+        "# },
+        (Enum, Indexed) => indoc! { r#"
+             {
+              Thing(T),
+              NotThing(bool),
+            }
+        "# },
+        (Enum, Named) => indoc! { r#"
+             {
+              Thing { t: T },
+              NotThing(bool),
+            }
+        "# },
+    });
+    input
+}
+
+fn matrix_expected(kind: Kind, fspec: FieldSpec, gen: Genericity) -> String {
+    match (kind, fspec, gen) {
+        (Struct, Indexed, Concrete) => indoc! { r#"
+            #[automatically_derived]
+            impl ::sappho_fconv::Extract<T> for Foo {
+                fn extract(self) -> Result<T, Self> {
+                    Ok(self.0)
+                }
+            }
+
+            #[automatically_derived]
+            impl ::sappho_fconv::Embed<T> for Foo {
+                fn embed(thing: T) -> Self {
+                    Foo(thing)
+                }
+            }
+        "# }
+        .to_string(),
+        (Struct, Indexed, Generic) => todo!(),
+        (Struct, Named, Concrete) => todo!(),
+        (Struct, Named, Generic) => todo!(),
+        (Enum, Indexed, Concrete) => todo!(),
+        (Enum, Indexed, Generic) => todo!(),
+        (Enum, Named, Concrete) => todo!(),
+        (Enum, Named, Generic) => todo!(),
+    }
 }
 
 fn parse_tokens(s: &str) -> TokenStream {
