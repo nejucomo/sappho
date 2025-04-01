adbd02e1885605da46692b62816978fdcb94fc49
Nate Wilcox <nejucomo+dev@gmail.com>
2025-04-01 10:21:34 -0700

    Flesh out `_east::Expr` structure; make `ListDef` first class.

--- east/src/expr.rs
+++ east/src/expr.rs
@@ -1,12 +1,48 @@
 use sappho_effect::Effect;
-use sappho_kast::ObjectDef;
+use sappho_identifier::RcId;
+use sappho_primval::PrimVal;
 
-use crate::EastProvider;
+use crate::{BoxWise, Let, ListDef, Match, ObjectDef};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Expr<FX>
 where
     FX: Effect,
 {
-    ObjectDef(ObjectDef<EastProvider, FX>),
+    Ref(RcId),
+    Prim(PrimVal),
+    ObjectDef(ObjectDef<FX>),
+    ListDef(ListDef<FX>),
+    Let(Let<FX>),
+    Match(Match<FX>),
+    Application(Application<FX>),
+    Lookup(Lookup<FX>),
+    Interaction(Interaction<FX>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Application<FX>
+where
+    FX: Effect,
+{
+    target: BoxWise<FX>,
+    argument: BoxWise<FX>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Lookup<FX>
+where
+    FX: Effect,
+{
+    target: BoxWise<FX>,
+    attrname: RcId,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Interaction<FX>
+where
+    FX: Effect,
+{
+    effect: FX,
+    target: BoxWise<FX>,
 }
--- east/src/lib.rs
+++ east/src/lib.rs
@@ -1,5 +1,20 @@
 mod expr;
 mod provider;
 
+use sappho_effect::{ProcEffect, PureEffect, QueryEffect};
+use sappho_listform::ListForm;
+
 pub use crate::expr::Expr;
 pub use crate::provider::EastProvider;
+
+pub type PureExpr = BoxWise<PureEffect>;
+pub type QueryExpr = BoxWise<QueryEffect>;
+pub type ProcExpr = BoxWise<ProcEffect>;
+
+pub type BoxWise<FX> = sappho_kast::BoxWise<EastProvider, FX>;
+pub type Let<FX> = sappho_kast::Let<EastProvider, FX>;
+pub type Match<FX> = sappho_kast::Match<EastProvider, FX>;
+pub type ObjectDef<FX> = sappho_kast::ObjectDef<EastProvider, FX>;
+pub type Wise<FX> = sappho_kast::Wise<EastProvider, FX>;
+
+pub type ListDef<FX> = ListForm<Wise<FX>, BoxWise<FX>>;
