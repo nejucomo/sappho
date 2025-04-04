b3c1cd25d9917b969395e6166dcfba638bd3b27e
Nate Wilcox <nejucomo+dev@gmail.com>
2025-04-04 06:01:53 -0700

    Add a TODO for pre-runtime syntactic scope slot assignment approach.

--- value/src/scope.rs
+++ value/src/scope.rs
@@ -4,6 +4,9 @@ use sappho_attrstack::AttrStack;
 
 use crate::Value;
 
+/// # TODO
+///
+/// Replace this with pre-runtime { RcId -> index } syntactic info and `Vec<Value>`
 #[derive(Clone, Debug, Default, PartialEq, Deref)]
 pub struct Scope(AttrStack<Value>);
 
