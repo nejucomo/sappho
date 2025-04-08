73ed367f2961b437b3bc1a9b984185366da2216d
Nate Wilcox <nejucomo+dev@gmail.com>
2025-04-08 14:40:41 -0700

    [no-verify] Remove `Unparse` requirement on `Parsable*`.

--- parsable/src/parsable.rs
+++ parsable/src/parsable.rs
@@ -1,12 +1,11 @@
 use std::fmt::Debug;
 
 use sappho_source::Source;
-use sappho_unparse::Unparse;
 
 use crate::error::Error;
 use crate::Parser;
 
-pub trait Parsable: Sized + Unparse + Debug {
+pub trait Parsable: Sized + Debug {
     fn load_and_parse<S>(source: S) -> Result<Self, Error>
     where
         Source: From<S>,
--- parsable/src/with.rs
+++ parsable/src/with.rs
@@ -1,11 +1,10 @@
 use std::fmt::Debug;
 
 use chumsky::Parser as _;
-use sappho_unparse::Unparse;
 
 use crate::Parser;
 
-pub trait ParsableWith<T>: Sized + Unparse + Debug {
+pub trait ParsableWith<T>: Sized + Debug {
     /// Consumers should call this to support parser debugging
     fn parser_with(param: T) -> impl Parser<Self> {
         Self::make_parser_with(param).map(move |parsed| dbg!(parsed))
