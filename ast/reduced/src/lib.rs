b2402eaac3d21f1af47294db7540724daf650ec0
nobody <nobody@nowhere.net>
2026-07-10 12:27:36 -0700

    Fix a doc-string bug.

--- ast/reduced/src/lib.rs
+++ ast/reduced/src/lib.rs
@@ -1,4 +1,4 @@
-//! The Reduced Abstract Syntax Tree is a subset of [`sappho-ast`] for evaluation
+//! The Reduced Abstract Syntax Tree is a subset of `sappho-ast` for evaluation
 
 mod expr;
 mod pattern;
